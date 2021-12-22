d783a21b3300ea4c7f804a0b7b915f9a963d70b2
andyR529 <andyjosue123.jr@gmail.com>
2021-12-22 14:40:46 -0600

    Cambios Nuevos

--- SistemaUniversidad.FrontEnd.Pr0/obj/Debug/net5.0/RazorDeclaration/Pages/CursosConProfesores - Copia.razor.g.cs
+++ SistemaUniversidad.FrontEnd.Pr0/obj/Debug/net5.0/RazorDeclaration/Pages/CursosConProfesores - Copia.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace SistemaUniversidad.FrontEnd.Pr0.Shared
+namespace SistemaUniversidad.FrontEnd.Pr0.Pages
 {
     #line hidden
     using System;
@@ -89,7 +89,8 @@ using SistemaUniversidad.FrontEnd.Pr0.Dtos;
 #line default
 #line hidden
 #nullable disable
-    public partial class SideBar : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/CursosConProfesore")]
+    public partial class CursosConProfesores___Copia : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -97,25 +98,27 @@ using SistemaUniversidad.FrontEnd.Pr0.Dtos;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 68 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\Shared\SideBar.razor"
+#line 74 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\Pages\CursosConProfesores - Copia.razor"
        
+    private CursosConProfesoreDto[] ListaDeCursosConProfesores;//Acá se guardará la lista de aulas
 
-    [Inject]
-    NavigationManager NavigationManager { get; set; }
+    private string UtlHost = "https://localhost:44365/api";
 
-    protected override void OnInitialized() => NavigationManager.LocationChanged += (s, e) => StateHasChanged();
-
-    bool IsActive(string href, NavLinkMatch navLinkMatch = NavLinkMatch.Prefix)
+    protected override async Task OnInitializedAsync() //Esto es lo primero que se ejecutará
     {
-        var relativePath = NavigationManager.ToBaseRelativePath(NavigationManager.Uri).ToLower();
-        return navLinkMatch == NavLinkMatch.All ? relativePath == href.ToLower() : relativePath.StartsWith(href.ToLower());
+        await ObtenerCursosConProfesores(); //con solo llamar a este método, ya se puede cargar la lista de aulas a nivel de la vista
     }
 
-    string GetActive(string href, NavLinkMatch navLinkMatch = NavLinkMatch.Prefix) => IsActive(href, navLinkMatch) ? "active" : "na";
+    private async Task ObtenerCursosConProfesores()
+    {
+        ListaDeCursosConProfesores = await Http.GetFromJsonAsync<CursosConProfesoreDto[]>($"{UtlHost}/CursosConProfesores");
+    }
 
 #line default
 #line hidden
 #nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IJSRuntime JSRuntime { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private HttpClient Http { get; set; }
     }
 }
 #pragma warning restore 1591
