819a16b9d6375bf196de2ef097a2dffc7fc268e0
andyR529 <andyjosue123.jr@gmail.com>
2021-12-22 14:20:20 -0600

    Cambios Nuevos

--- SistemaUniversidad.FrontEnd.Pr0/Dtos/CicloLectivoDto.cs
+++ SistemaUniversidad.FrontEnd.Pr0/Dtos/CicloLectivoDto.cs
@@ -7,14 +7,19 @@ using System.Threading.Tasks;
 namespace SistemaUniversidad.FrontEnd.Pr0.Dtos
 {
     public class CicloLectivoDto
-
     {
         [Required(ErrorMessage = "{0} es un campo obligatorio")]
         [MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
-        public string NumeroAula { get; set; }
+        public string NumeroCiclo { get; set; }
 
         [Required(ErrorMessage = "{0} es un campo obligatorio")]
-        public string NombreAula { get; set; }
+        public string NombreCiclo { get; set; }
+
+        [Required(ErrorMessage = "La fecha de inicio es obligatoria")]
+        public DateTime FechaInicio { get; set; }
+
+        [Required(ErrorMessage = "La fecha de fin es obligatoria")]
+        public DateTime FechaFin { get; set; }
 
     }
 }
