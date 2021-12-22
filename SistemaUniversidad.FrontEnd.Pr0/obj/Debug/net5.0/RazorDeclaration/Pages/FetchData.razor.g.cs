a81fb633c5841fd632d6e9126b848a751e437046
andyR529 <andyjosue123.jr@gmail.com>
2021-12-22 13:48:57 -0600

    Arreglos

--- SistemaUniversidad.FrontEnd.Pr0/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ SistemaUniversidad.FrontEnd.Pr0/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace SistemaUniversidad.FrontEnd.Pr0.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 1 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 2 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 3 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 4 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 5 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 6 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 7 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 8 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 9 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using SistemaUniversidad.FrontEnd.Pr0;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 10 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using SistemaUniversidad.FrontEnd.Pr0.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
+#line 11 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\_Imports.razor"
 using SistemaUniversidad.FrontEnd.Pr0.Dtos;
 
 #line default
@@ -98,7 +98,7 @@ using SistemaUniversidad.FrontEnd.Pr0.Dtos;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 37 "C:\Users\andyj\Escritorio\Progra\BD2\SistemaUniversidad.FrontEnd.Pr0\Pages\FetchData.razor"
+#line 37 "C:\Users\andyj\Escritorio\Progra\Sistema Universidad\SistemaUniversidad.FrontEnd\SistemaUniversidad.FrontEnd.Pr0\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
